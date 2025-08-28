62cc6bbc4f137c993d7fb96cc0a8144b72eb5a2c
IT24101981 <it24101981@my.sliit.lk>
2025-08-28 23:24:37 +0530

    Update IT24101981.R

--- Lab Sheet 05/IT24101981.R
+++ Lab Sheet 05/IT24101981.R
@@ -1,3 +1,29 @@
-setwd ("C:\\Users\\it24101981\\Desktop\\IT24101981")
-Delivery_Times<- read.table("Exercise - Lab 05.txt", header=TRUE, sep=",")
+setwd("C:\\Users\\malsh\\Desktop\\IT24101981")
+Delivery_Times <- read.table("Exercise - Lab 05.txt", header = TRUE)
+
+colnames(Delivery_Times) <- "Delivery_Time"
+head(Delivery_Times)
+str(Delivery_Times)
+
+
+breaks <- seq(20, 70, length.out = 10)
+hist(Delivery_Times$Delivery_Time,right = FALSE,breaks = breaks,main = "Histogram of Delivery Times",xlab = "Delivery Time",ylab = "Frequency")
+
+
+hist_data <- hist(Delivery_Times$Delivery_Time, breaks = breaks, right = FALSE, plot = FALSE)
+frequencies <- hist_data$counts
+cum_freq <- cumsum(frequencies)
+print(frequencies)
+print(cum_freq)
+
+
+
+midpoints <- hist_data$mids
+plot(midpoints, cum_freq, type = "b",
+     main = "Cumulative Frequency Polygon for Delivery Time",
+     xlab = "Delivery Time (minutes)",
+     ylab = "Cumulative Frequency",
+     pch = 16)
+
+
 
