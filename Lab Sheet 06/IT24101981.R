a73a0dda978e96824c52345940d4a00e6a069b07
IT24101981 <it24101981@my.sliit.lk>
2025-09-15 14:17:16 +0530

    ps lab06

---
+++ Lab Sheet 06/IT24101981.R
@@ -0,0 +1,24 @@
+setwd("C:\\Users\\malsh\\Desktop\\Lab6")
+
+
+#Q1
+
+#1
+#Binomial Distribution
+
+#2
+1-pbinom(47,50,0.85,lower.tail =FALSE)
+
+
+#Q2
+
+#1
+#number of customer calls per hour
+
+#2
+#poisson Distribution
+
+#3
+dpois(15,12)
+
+
